ae80828006ee5fa8b4c3149ba59445d5f4e63b94
Kanishke-Mac <kanishkegb@gwu.edu>
2018-03-27 11:16:10 -0400

    added new c libraries

---
+++ examples/sensor/build/libvnc/CMakeFiles/libvnc.dir/DependInfo.cmake
@@ -0,0 +1,38 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/kanishke/Downloads/vnproglib-1.1/c/src/vn/conv.c" "/Users/kanishke/Downloads/vnproglib-1.1/c/examples/sensor/build/libvnc/CMakeFiles/libvnc.dir/src/vn/conv.c.o"
+  "/Users/kanishke/Downloads/vnproglib-1.1/c/src/vn/error.c" "/Users/kanishke/Downloads/vnproglib-1.1/c/examples/sensor/build/libvnc/CMakeFiles/libvnc.dir/src/vn/error.c.o"
+  "/Users/kanishke/Downloads/vnproglib-1.1/c/src/vn/error_detection.c" "/Users/kanishke/Downloads/vnproglib-1.1/c/examples/sensor/build/libvnc/CMakeFiles/libvnc.dir/src/vn/error_detection.c.o"
+  "/Users/kanishke/Downloads/vnproglib-1.1/c/src/vn/math/matrix.c" "/Users/kanishke/Downloads/vnproglib-1.1/c/examples/sensor/build/libvnc/CMakeFiles/libvnc.dir/src/vn/math/matrix.c.o"
+  "/Users/kanishke/Downloads/vnproglib-1.1/c/src/vn/math/vector.c" "/Users/kanishke/Downloads/vnproglib-1.1/c/examples/sensor/build/libvnc/CMakeFiles/libvnc.dir/src/vn/math/vector.c.o"
+  "/Users/kanishke/Downloads/vnproglib-1.1/c/src/vn/protocol/spi.c" "/Users/kanishke/Downloads/vnproglib-1.1/c/examples/sensor/build/libvnc/CMakeFiles/libvnc.dir/src/vn/protocol/spi.c.o"
+  "/Users/kanishke/Downloads/vnproglib-1.1/c/src/vn/protocol/upack.c" "/Users/kanishke/Downloads/vnproglib-1.1/c/examples/sensor/build/libvnc/CMakeFiles/libvnc.dir/src/vn/protocol/upack.c.o"
+  "/Users/kanishke/Downloads/vnproglib-1.1/c/src/vn/protocol/upackf.c" "/Users/kanishke/Downloads/vnproglib-1.1/c/examples/sensor/build/libvnc/CMakeFiles/libvnc.dir/src/vn/protocol/upackf.c.o"
+  "/Users/kanishke/Downloads/vnproglib-1.1/c/src/vn/sensors.c" "/Users/kanishke/Downloads/vnproglib-1.1/c/examples/sensor/build/libvnc/CMakeFiles/libvnc.dir/src/vn/sensors.c.o"
+  "/Users/kanishke/Downloads/vnproglib-1.1/c/src/vn/sensors/compositedata.c" "/Users/kanishke/Downloads/vnproglib-1.1/c/examples/sensor/build/libvnc/CMakeFiles/libvnc.dir/src/vn/sensors/compositedata.c.o"
+  "/Users/kanishke/Downloads/vnproglib-1.1/c/src/vn/sensors/ezasyncdata.c" "/Users/kanishke/Downloads/vnproglib-1.1/c/examples/sensor/build/libvnc/CMakeFiles/libvnc.dir/src/vn/sensors/ezasyncdata.c.o"
+  "/Users/kanishke/Downloads/vnproglib-1.1/c/src/vn/sensors/searcher.c" "/Users/kanishke/Downloads/vnproglib-1.1/c/examples/sensor/build/libvnc/CMakeFiles/libvnc.dir/src/vn/sensors/searcher.c.o"
+  "/Users/kanishke/Downloads/vnproglib-1.1/c/src/vn/util.c" "/Users/kanishke/Downloads/vnproglib-1.1/c/examples/sensor/build/libvnc/CMakeFiles/libvnc.dir/src/vn/util.c.o"
+  "/Users/kanishke/Downloads/vnproglib-1.1/c/src/vn/xplat/criticalsection.c" "/Users/kanishke/Downloads/vnproglib-1.1/c/examples/sensor/build/libvnc/CMakeFiles/libvnc.dir/src/vn/xplat/criticalsection.c.o"
+  "/Users/kanishke/Downloads/vnproglib-1.1/c/src/vn/xplat/event.c" "/Users/kanishke/Downloads/vnproglib-1.1/c/examples/sensor/build/libvnc/CMakeFiles/libvnc.dir/src/vn/xplat/event.c.o"
+  "/Users/kanishke/Downloads/vnproglib-1.1/c/src/vn/xplat/serialport.c" "/Users/kanishke/Downloads/vnproglib-1.1/c/examples/sensor/build/libvnc/CMakeFiles/libvnc.dir/src/vn/xplat/serialport.c.o"
+  "/Users/kanishke/Downloads/vnproglib-1.1/c/src/vn/xplat/thread.c" "/Users/kanishke/Downloads/vnproglib-1.1/c/examples/sensor/build/libvnc/CMakeFiles/libvnc.dir/src/vn/xplat/thread.c.o"
+  "/Users/kanishke/Downloads/vnproglib-1.1/c/src/vn/xplat/time.c" "/Users/kanishke/Downloads/vnproglib-1.1/c/examples/sensor/build/libvnc/CMakeFiles/libvnc.dir/src/vn/xplat/time.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../../../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
